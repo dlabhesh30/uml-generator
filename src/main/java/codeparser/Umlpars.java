f709666bf0ff97036cc473e2face5e7716b51f63
dlabhesh30 <dlabhesh@gmail.com>
2017-04-18 17:23:56 -0700

    adding parameter tomethod

--- src/main/java/codeparser/Umlpars.java
+++ src/main/java/codeparser/Umlpars.java
@@ -17,7 +17,8 @@ public class Umlpars {
                 // change the name of the method to upper case
                 n.setName(n.getNameAsString().toUpperCase());
 
-            
+                // add a new parameter to the method
+                n.addParameter("int", "value");
             }
         }
        
