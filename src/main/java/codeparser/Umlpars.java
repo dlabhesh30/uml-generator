c1c72c64b8a48eb38da7f538da1ca33645a93527
dlabhesh30 <dlabhesh@gmail.com>
2017-04-18 17:19:22 -0700

    shifting code to uml pars for modularity

--- src/main/java/codeparser/Umlpars.java
+++ src/main/java/codeparser/Umlpars.java
@@ -2,4 +2,10 @@ package codeparser;
 
 public class Umlpars {
 
-}
+
+    public static void main(String[] args) throws Exception {
+        // parse a file
+        CompilationUnit cu = JavaParser.parse(new File("G:\\Study\\M.S\\202\\UML-Parser\\resources\\input_files\\first.java"));
+
+        // visit and change the me
+    }
